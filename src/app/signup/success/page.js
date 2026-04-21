30a9f9d831489f99cc4f6a115a0197ed5614426c
Fizza-awan007 <fizzaawan817@gmail.com>
2026-04-21 18:00:16 +0500

    Added docker file

--- src/app/signup/success/page.js
+++ src/app/signup/success/page.js
@@ -1,10 +1,11 @@
+import { Suspense } from "react";
 import AccountSuccessPage from "@/components/features/AccountSuccessPage";
 
-export const metadata = {
-  title: "Congratulations! | Funsival",
-  description: "Your Funsival account has been created successfully.",
-};
 
 export default function SignupSuccess() {
-  return <AccountSuccessPage />;
+  return (
+    <Suspense>
+      <AccountSuccessPage />
+    </Suspense>
+  );
 }
